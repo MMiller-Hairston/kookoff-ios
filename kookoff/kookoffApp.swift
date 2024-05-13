e28881f4c9475db4bf4090a0210438bb581d29f0
Michael Miller-Hairston <michael.millerhairston@gmail.com>
2024-05-12 21:42:09 -0700

    Initial Commit

---
+++ kookoff/kookoffApp.swift
@@ -0,0 +1,17 @@
+//
+//  kookoffApp.swift
+//  kookoff
+//
+//  Created by Michael Miller on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct kookoffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
